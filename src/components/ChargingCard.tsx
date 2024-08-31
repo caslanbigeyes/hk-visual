66d534dfb45d80ad1465eaa067d124896156d2b7
blue <53009552+caslanbigeyes@users.noreply.github.com>
2024-09-01 00:28:43 +0800

    add

--- src/components/ChargingCard.tsx
+++ src/components/ChargingCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Line } from 'react-chartjs-2';
 import CustomLineChart from '@/components/CustomLineChart'
 
 import CustomImage from '@/components/CustomImage';
